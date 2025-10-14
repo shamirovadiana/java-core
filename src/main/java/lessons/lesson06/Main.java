8612ed7c107b899fa73015408dae288060941879
shamirovadiana <shamirovadiana02@gmail.com>
2025-10-15 03:29:13 +0500

    lesson06

---
+++ src/main/java/lessons/lesson06/Main.java
@@ -0,0 +1,13 @@
+package lessons.lesson06;
+
+public class Main {
+    public static void main(String[] args){
+        Airbus airbus = new Airbus();
+        airbus.setSeatsStatus();
+        airbus.getSeatsInfo();
+        airbus.bookSeat(20, "Fio", "Phone", "Email");
+        airbus.getSeatInfo(20);
+        airbus.cancelReservation(20);
+        airbus.getSeatInfo(20);
+    }
+}
