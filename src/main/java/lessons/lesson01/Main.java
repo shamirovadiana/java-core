2e05e162fe795f21c972f7edb69280475695360f
shamirovadiana <shamirovadiana02@gmail.com>
2025-10-14 11:00:19 +0500

    first commit

---
+++ src/main/java/lessons/lesson01/Main.java
@@ -0,0 +1,8 @@
+package lessons.lesson01;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Homework for first lesson of internship was not assigned");
+    }
+
+}
